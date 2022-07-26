b87e05f4fb2559c31eabbca544d21985dca6ab60
Syfer05 <syfer@gmx.fr>
2022-07-26 23:47:12 +0200

    Add calcul_imc() et conseil()

--- lib.py
+++ lib.py
@@ -1,2 +1,18 @@
-def calcul_imc(values):
-    return null
+
+def calcul_imc():
+    poids = int(input ("Quel est votre poids en kg\n"))
+    taille = (int(input("Quelle est votre taille en cm\n")))/100
+    resultat = poids / (taille**2)
+    print("Votre IMC est de :", resultat, "\n\n")
+
+
+def conseil():
+        poids = int(input("Quel est votre poids en kg\n"))
+        taille = (int(input("Quelle est votre taille en cm\n"))) / 100
+        resultat = poids / (taille ** 2)
+        if resultat >= 25:
+            print("Perdez du poids svp!")
+        elif resultat <18:
+            print("Votre corps est en limite de fonctionnement")
+        else:
+            print("Continuer comme cela!")
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from lib import calcul_imc
+from lib import conseil
 
 #-Calcul de l'IMC
-#-Analyse deu poids en fct de personnes demême caracteristiques (Comparaison moyenne sur BDD)
+#-Analyse du poids en fct de personnes demême caracteristiques (Comparaison moyenne sur BDD)
 #-Annonce de régime selon le poids actuel (3 programmes 1 Je dois groissir, 2 dois maigrir, 3 change rien)
 #-Gérer un historique des mesures sur le temps du client (pas forcément sur la BDD, le local peut-être suffisant au début)
 
 
-print("Menu")
 
+
+def menu():
+    choix = input("Veuillez choisir votre fonctionnalité!\n 1:Calcul IMC \n 2:Comparaison \n 3:Conseil \n 4:Historique \n q:Quitter \n")
+    if choix == "1":
+        calcul_imc()
+    elif choix == "2":
+        pass
+    elif choix == "3":
+        conseil()
+    elif choix == "4":
+        pass
+    elif choix == "q":
+        pass
+    menu()
 #def calcul_imc():
+
+menu()
